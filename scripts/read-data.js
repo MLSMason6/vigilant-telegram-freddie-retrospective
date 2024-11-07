965f8a31669d6c3e68afe7b3df372ea7d338f5db
MLSMason6 <mschrader1@una.edu>
2024-11-07 09:55:53 -0600

    Update read-data.js

--- scripts/read-data.js
+++ scripts/read-data.js
@@ -8,13 +8,13 @@
 // });
 
 
-$("button").click(function() {
-    $.getJSON("https://mlsmason6.github.io/mlsmason6/data.json", function(result) {
-        $.each(result, function(i, field) {
-            $("div").append(field + " ");
-        });
+$("button").click(function(){
+    $.getJSON("https://barrycumbie.github.io/barrycumbie/data.json", function(result){
+      $.each(result, function(i, field){
+        $("div").append(field + " ");
+      });
     });
-});
+  });
 
 
 // $.getJSON( "https://barrycumbie.github.io/barrycumbie/data.js", function( data ) {
