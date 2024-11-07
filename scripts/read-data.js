a4f1baf8a8793df960293feb0ec9877c9637b7b3
MLSMason6 <mschrader1@una.edu>
2024-11-07 09:45:35 -0600

    Update read-data.js

--- scripts/read-data.js
+++ scripts/read-data.js
@@ -9,7 +9,7 @@
 
 
 $("button").click(function(){
-    $.getJSON("https://barrycumbie.github.io/barrycumbie/data.json", function(result){
+    $.getJSON("https://mlsmason6.github.io/mlsmason6/data.json", function(result){
       $.each(result, function(i, field){
         $("div").append(field + " ");
       });
